212f4d6a1d3101a5350caca7a8c5127eb90f06f6
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2022-09-15 00:11:06 -0300

    feat: first page

---
+++ src/presentation/pages/login/login.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Syles from './login-styles.scss'
+
+const Login: React.FC = () => {
+  return <div className={Syles.login}>Clean TDD - Start</div>
+}
+
+export default Login
